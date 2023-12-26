161be24f5824b09046c3a93fe0782f2d8037c4d0
celso <c.el@live.com>
2023-12-26 16:43:32 -0300

    feat: added run_amzoncat_13k.sh.

--- run_amzoncat_13k.sh
+++ run_amzoncat_13k.sh
@@ -3,13 +3,13 @@ DATA=AmazonCat-13k
 FOLD=0
 
 train_start=$(date)
-python src/main.py --lr 1e-4 --epoch 5 --dataset $DATA --fold $FOLD --swa --swa_warmup 2 --swa_step 10000 --batch 16 --eval_step 20000
-python src/main.py --lr 1e-4 --epoch 5 --dataset $DATA --fold $FOLD --swa --swa_warmup 2 --swa_step 10000 --batch 16 --eval_step 20000 --bert roberta
-python src/main.py --lr 1e-4 --epoch 5 --dataset $DATA --fold $FOLD --swa --swa_warmup 2 --swa_step 10000 --batch 32 --eval_step 20000 --bert xlnet --max_len 128
+python3 src/main.py --lr 1e-4 --epoch 5 --dataset $DATA --fold $FOLD --swa --swa_warmup 2 --swa_step 10000 --batch 16 --eval_step 20000
+python3 src/main.py --lr 1e-4 --epoch 5 --dataset $DATA --fold $FOLD --swa --swa_warmup 2 --swa_step 10000 --batch 16 --eval_step 20000 --bert roberta
+python3 src/main.py --lr 1e-4 --epoch 5 --dataset $DATA --fold $FOLD --swa --swa_warmup 2 --swa_step 10000 --batch 32 --eval_step 20000 --bert xlnet --max_len 128
 train_end=$(date)
 
 predict_start=$(date)
-python src/ensemble.py --dataset $DATA
+python3 src/ensemble.py --dataset $DATA
 predict_end=$(date)
 
 
